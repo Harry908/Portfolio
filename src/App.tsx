3062ce5293c70d159da140d557c9967cda086f76
Huy Ky <huyky26@gmail.com>
2025-05-20 23:55:28 -0700

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import LogoSection from "./components/LogoSection"
-import NavBar from "./components/Navbar"
+import NavBar from "./components/NavBar"
 import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import Showcase from "./sections/Showcase"
