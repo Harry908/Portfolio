a86e9b917102cebf88c451d516a855454dcb004f
Huy Ky <huyky26@gmail.com>
2025-05-23 00:34:56 -0700

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio/' : '/',
+export default defineConfig(() => ({
+  base: '/Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
